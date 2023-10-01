23e84a8b7a30f46f95a3cf70c387a14a409ad719
harshil chudasama <harshilc71@gmail.com>
2023-10-01 22:23:04 +0530

    change in api path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 // cSpell:word todos
 import React, { useEffect, useState } from "react";
-import {
-	CaretSortIcon,
-	ChevronDownIcon,
-	DotsHorizontalIcon,
-} from "@radix-ui/react-icons";
+import { CaretSortIcon, ChevronDownIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
 import {
 	ColumnDef,
 	ColumnFiltersState,
@@ -30,23 +26,8 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { useSnackbar } from "notistack";
 import { TbEdit } from "react-icons/tb/index";
@@ -63,8 +44,7 @@ import {
 	AlertDialogHeader,
 	AlertDialogTitle,
 	AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  
+} from "@/components/ui/alert-dialog";
 
 export type Payment = {
 	_id?: string;
@@ -84,11 +64,8 @@ const TodoPage = () => {
 	const [todo, setTodo] = useState<string>("");
 	const [theme, setTheme] = useState<string>("light");
 	const [sorting, setSorting] = React.useState<SortingState>([]);
-	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-		[],
-	);
-	const [columnVisibility, setColumnVisibility] =
-		React.useState<VisibilityState>({});
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+	const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
 	const [rowSelection, setRowSelection] = React.useState({});
 	const [singleData, setSingleData] = useState({
 		title: "",
@@ -108,39 +85,26 @@ const TodoPage = () => {
 					aria-label="Select all"
 				/>
 			),
-			cell: ({ row }) => (
-				<Checkbox
-					checked={row.getIsSelected()}
-					onCheckedChange={(value) => row.toggleSelected(!!value)}
-					aria-label="Select row"
-				/>
-			),
+			cell: ({ row }) => <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} aria-label="Select row" />,
 			enableSorting: false,
 			enableHiding: false,
 		},
 		{
 			accessorKey: "title",
 			header: "Title",
-			cell: ({ row }) => (
-				<div className="capitalize">{row.getValue("title")}</div>
-			),
+			cell: ({ row }) => <div className="capitalize">{row.getValue("title")}</div>,
 		},
 		{
 			accessorKey: "category",
 			header: ({ column }) => {
 				return (
-					<Button
-						variant="ghost"
-						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-					>
+					<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
 						Category
 						<CaretSortIcon className="ml-2 h-4 w-4" />
 					</Button>
 				);
 			},
-			cell: ({ row }) => (
-				<div className="lowercase">{row.getValue("category")}</div>
-			),
+			cell: ({ row }) => <div className="lowercase">{row.getValue("category")}</div>,
 		},
 		{
 			accessorKey: "amount",
@@ -173,15 +137,11 @@ const TodoPage = () => {
 						</DropdownMenuTrigger>
 						<DropdownMenuContent align="end">
 							<DropdownMenuLabel>Actions</DropdownMenuLabel>
-							<DropdownMenuItem
-								onClick={() => navigator.clipboard.writeText(payment._id)}
-							>
+							<DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment._id)}>
 								<MdContentCopy className="me-2" /> Copy payment ID
 							</DropdownMenuItem>
 							<DropdownMenuSeparator />
-							<DropdownMenuItem
-								onClick={async () => await getEditData(payment._id)}
-							>
+							<DropdownMenuItem onClick={async () => await getEditData(payment._id)}>
 								<TbEdit className="me-2" /> Edit
 							</DropdownMenuItem>
 							<DropdownMenuItem onClick={async () => await handleDeleteData(payment._id)}>
@@ -283,6 +243,29 @@ const TodoPage = () => {
 				variant: "error",
 			});
 		} else if (singleData?._id) {
+			const response = await fetch(`${process.env.API_PATH}/expense/${singleData._id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(singleData),
+			});
+			const responseData = await response.json();
+			if (responseData.success) {
+				setSingleData({
+					title: "",
+					amount: 0,
+					category: "",
+				});
+				closeSnackbar();
+				setOpen(false);
+				await getData();
+				return enqueueSnackbar("Expense updated successfully", {
+					variant: "success",
+				});
+			} else {
+				return enqueueSnackbar(responseData.message, { variant: "error" });
+			}
 		} else {
 			const response = await fetch(`${process.env.API_PATH}/expense/create`, {
 				method: "POST",
@@ -318,11 +301,7 @@ const TodoPage = () => {
 
 	return (
 		<div className="w-screen h-screen p-0 m-0">
-			<div
-				className={`w-100 mx-auto p-4 ${
-					theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"
-				}`}
-			>
+			<div className={`w-100 mx-auto p-4 ${theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"}`}>
 				<header className="p-2 mb-2 rounded flex justify-between items-center">
 					<h1 className="text-4xl font-bold">Todo App</h1>
 					{/* <button
@@ -342,10 +321,7 @@ const TodoPage = () => {
 							<DialogContent className="sm:max-w-[425px]">
 								<DialogHeader>
 									<DialogTitle>Edit profile</DialogTitle>
-									<DialogDescription>
-										Make changes to your profile here. Click save when
-										you&apos;re done.
-									</DialogDescription>
+									<DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
 								</DialogHeader>
 								<div className="grid gap-4 py-4">
 									<div className="grid grid-cols-4 items-center gap-4">
@@ -455,9 +431,7 @@ const TodoPage = () => {
 					<Input
 						placeholder="Filter title..."
 						value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
-						onChange={(event) =>
-							table.getColumn("title")?.setFilterValue(event.target.value)
-						}
+						onChange={(event) => table.getColumn("title")?.setFilterValue(event.target.value)}
 						className="max-w-sm"
 					/>
 					<DropdownMenu>
@@ -476,9 +450,7 @@ const TodoPage = () => {
 											key={column.id}
 											className="capitalize"
 											checked={column.getIsVisible()}
-											onCheckedChange={(value) =>
-												column.toggleVisibility(!!value)
-											}
+											onCheckedChange={(value) => column.toggleVisibility(!!value)}
 										>
 											{column.id}
 										</DropdownMenuCheckboxItem>
@@ -495,12 +467,7 @@ const TodoPage = () => {
 									{headerGroup.headers.map((header) => {
 										return (
 											<TableHead key={header.id}>
-												{header.isPlaceholder
-													? null
-													: flexRender(
-															header.column.columnDef.header,
-															header.getContext(),
-													  )}
+												{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
 											</TableHead>
 										);
 									})}
@@ -510,26 +477,15 @@ const TodoPage = () => {
 						<TableBody>
 							{table.getRowModel().rows?.length ? (
 								table.getRowModel().rows.map((row) => (
-									<TableRow
-										key={row.id}
-										data-state={row.getIsSelected() && "selected"}
-									>
+									<TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
 										{row.getVisibleCells().map((cell) => (
-											<TableCell key={cell.id}>
-												{flexRender(
-													cell.column.columnDef.cell,
-													cell.getContext(),
-												)}
-											</TableCell>
+											<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
 										))}
 									</TableRow>
 								))
 							) : (
 								<TableRow>
-									<TableCell
-										colSpan={columns.length}
-										className="h-24 text-center"
-									>
+									<TableCell colSpan={columns.length} className="h-24 text-center">
 										No results.
 									</TableCell>
 								</TableRow>
@@ -539,24 +495,13 @@ const TodoPage = () => {
 				</div>
 				<div className="flex items-center justify-end space-x-2 py-4">
 					<div className="flex-1 text-sm text-muted-foreground">
-						{table.getFilteredSelectedRowModel().rows.length} of{" "}
-						{table.getFilteredRowModel().rows.length} row(s) selected.
+						{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
 					</div>
 					<div className="space-x-2">
-						<Button
-							variant="outline"
-							size="sm"
-							onClick={() => table.previousPage()}
-							disabled={!table.getCanPreviousPage()}
-						>
+						<Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
 							Previous
 						</Button>
-						<Button
-							variant="outline"
-							size="sm"
-							onClick={() => table.nextPage()}
-							disabled={!table.getCanNextPage()}
-						>
+						<Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
 							Next
 						</Button>
 					</div>
