037f9d34c4d1920c4596553e784d1396b1626c69
harshil <harshilc71@gmail.com>
2023-10-08 12:43:28 +0530

    register & login page ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -41,22 +41,51 @@ export default function LoginPage() {
 			.catch((err) => console.error(err));
 	}
 	return (
-		<div className="h-screen w-screen flex justify-center items-center bg-black text-white">
-			<form onSubmit={handleSubmit}>
-				<h1 className="h1 py-2 px-1 font-bold text-center">Login Page</h1>
-				<div className="p-2">
-					<label htmlFor="emailOrUsername">Email or Username</label>
-					<input type="text" name="emailOrUsername" id="emailOrUsername" className="ms-1 bg-slate-700" onChange={handleInputChange} />
+		<div className="h-screen w-screen flex justify-center items-center bg-gray-900 text-white">
+			<div
+				className="absolute inset-0 bg-cover bg-center z-0"
+				style={{
+					backgroundImage:
+						"url('https://img.freepik.com/free-photo/beautiful-nature-landscape-with-mountains-lake_23-2150705872.jpg?t=st=1696747532~exp=1696751132~hmac=d049e532cfb304a7a0cfa3d1dcfb434eb01a972d2c1c4a0045003bcfe1961460&w=1060')",
+				}}
+			/>
+			<form onSubmit={handleSubmit} className="bg-gray-800 p-10 rounded-lg shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 z-10 w-1/4">
+				<h1 className="text-3xl font-bold text-center mb-4">Login Page</h1>
+				<div className="mb-4">
+					<label htmlFor="emailOrUsername" className="block font-medium mb-2">
+						Email or Username
+					</label>
+					<input
+						type="text"
+						name="emailOrUsername"
+						id="emailOrUsername"
+						className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white"
+						onChange={handleInputChange}
+					/>
 				</div>
-				<div className="p-2">
-					<label htmlFor="password">Password</label>
-					<input type={showPassword ? "text" : "password"} name="password" id="password" className="ms-1 bg-slate-700" onChange={handleInputChange} />
-					<button className="ms-1" type="button" onClick={() => setShowPassword((prev) => (prev ? false : true))}>
-						{showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
-					</button>
+				<div className="mb-4">
+					<label htmlFor="password" className="block font-medium mb-2">
+						Password
+					</label>
+					<div className="relative">
+						<input
+							type={showPassword ? "text" : "password"}
+							name="password"
+							id="password"
+							className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white"
+							onChange={handleInputChange}
+						/>
+						<button
+							className="absolute top-1/2 right-2 transform -translate-y-1/2"
+							type="button"
+							onClick={() => setShowPassword((prev) => (prev ? false : true))}
+						>
+							{showPassword ? <AiFillEye className="text-gray-400" /> : <AiFillEyeInvisible className="text-gray-400" />}
+						</button>
+					</div>
 				</div>
-				<div className="p-2">
-					<button type="submit" className="py-1 px-2 rounded bg-gray-700">
+				<div className="mb-4">
+					<button type="submit" className="w-full py-2 px-4 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors duration-300">
 						Submit
 					</button>
 				</div>
--- pages/register.tsx
+++ pages/register.tsx
@@ -4,7 +4,8 @@ import React from "react";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 export default function RegisterPage() {
-	const [showPassword, setShowPassword] = React.useState(false);
+	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+	const [showPassword, setShowPassword] = React.useState([false, false]);
 	const [data, setData] = React.useState({
 		username: "",
 		email: "",
@@ -12,12 +13,10 @@ export default function RegisterPage() {
 		repeat_password: "",
 	});
 	const router = useRouter();
-	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
 	async function handleSubmit(e: React.FormEvent) {
 		e.preventDefault();
-		// username email
-		console.log(data);
+		
 		await fetch(`${process.env.API_PATH}/auth/register`, {
 			method: "POST",
 			body: JSON.stringify(data),
@@ -26,60 +25,96 @@ export default function RegisterPage() {
 			},
 		})
 			.then((res) => res.json())
-			.then((res) => {
-				if (res.success) {
+			.then((response) => {
+				if (response.success) {
+					enqueueSnackbar(response.message, { variant: "success" });
 					return router.push("/login");
 				} else {
-					return enqueueSnackbar(res.message, { variant: "error" });
+					enqueueSnackbar(response.message, { variant: "error" });
+					return false;
 				}
-			});
+			})
+			.catch((err) => console.error(err));
 	}
 	return (
-		<div className="h-screen w-screen flex justify-center items-center bg-black text-white">
-			<form onSubmit={handleSubmit}>
-				<h1 className="h1 py-2 px-1 font-bold text-center">Register Page</h1>
-				<div className="p-2">
-					<label htmlFor="username">Username</label>
+		<div className="h-screen w-screen flex justify-center items-center bg-gray-900 text-white">
+			<div
+				className="absolute inset-0 bg-cover bg-center z-0"
+				style={{
+					backgroundImage:
+						"url('https://img.freepik.com/free-photo/beautiful-nature-landscape-with-mountains-lake_23-2150705872.jpg?t=st=1696747532~exp=1696751132~hmac=d049e532cfb304a7a0cfa3d1dcfb434eb01a972d2c1c4a0045003bcfe1961460&w=1060')",
+				}}
+			/>
+			<form onSubmit={handleSubmit} className="bg-gray-800 p-10 rounded-lg shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 z-10 w-1/4">
+				<h1 className="text-3xl font-bold text-center mb-5">Register Page</h1>
+				<div className="mb-4">
+					<label htmlFor="username" className="block text-gray-300 font-bold mb-1">
+						Username
+					</label>
 					<input
 						type="text"
 						name="username"
 						id="username"
-						className="ms-1 bg-slate-700"
+						className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white"
 						onChange={(e) => setData({ ...data, username: e.target.value })}
 					/>
 				</div>
-				<div className="p-2">
-					<label htmlFor="email">Email</label>
-					<input type="email" name="email" id="email" className="ms-1 bg-slate-700" onChange={(e) => setData({ ...data, email: e.target.value })} />
-				</div>
-				<div className="p-2">
-					<label htmlFor="password">Password</label>
+				<div className="mb-4">
+					<label htmlFor="email" className="block text-gray-300 font-bold mb-1">
+						Email
+					</label>
 					<input
-						type={showPassword ? "text" : "password"}
-						name="password"
-						id="password"
-						className="ms-1 bg-slate-700"
-						onChange={(e) => setData({ ...data, password: e.target.value })}
+						type="email"
+						name="email"
+						id="email"
+						className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white"
+						onChange={(e) => setData({ ...data, email: e.target.value })}
 					/>
-					<button className="ms-1" type="button" onClick={() => setShowPassword((prev) => (prev ? false : true))}>
-						{showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
-					</button>
 				</div>
-				<div className="p-2">
-					<label htmlFor="repeat_password">Confirm Password</label>
-					<input
-						type={showPassword ? "text" : "password"}
-						name="repeat_password"
-						id="repeat_password"
-						className="ms-1 bg-slate-700"
-						onChange={(e) => setData({ ...data, repeat_password: e.target.value })}
-					/>
-					<button className="ms-1" type="button" onClick={() => setShowPassword((prev) => (prev ? false : true))}>
-						{showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
-					</button>
+				<div className="mb-4">
+					<label htmlFor="password" className="block text-gray-300 font-bold mb-1">
+						Password
+					</label>
+					<div className="relative">
+						<input
+							type={showPassword[0] ? "text" : "password"}
+							name="password"
+							id="password"
+							className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white pr-10"
+							onChange={(e) => setData({ ...data, password: e.target.value })}
+						/>
+						<button
+							className="absolute top-1/2 right-2 transform -translate-y-1/2"
+							type="button"
+							onClick={() => setShowPassword((prev) => [prev[0] ? false : true, prev[1]])}
+						>
+							{showPassword[0] ? <AiFillEye /> : <AiFillEyeInvisible />}
+						</button>
+					</div>
 				</div>
-				<div className="p-2 text-center">
-					<button type="submit" className="py-1 px-2 rounded bg-gray-700">
+				<div className="mb-4">
+					<label htmlFor="repeat_password" className="block text-gray-300 font-bold mb-1">
+						Confirm Password
+					</label>
+					<div className="relative">
+						<input
+							type={showPassword[1] ? "text" : "password"}
+							name="repeat_password"
+							id="repeat_password"
+							className="w-full px-3 py-2 rounded-lg bg-gray-700 text-white pr-10"
+							onChange={(e) => setData({ ...data, repeat_password: e.target.value })}
+						/>
+						<button
+							className="absolute top-1/2 right-2 transform -translate-y-1/2"
+							type="button"
+							onClick={() => setShowPassword((prev) => [prev[0], prev[1] ? false : true])}
+						>
+							{showPassword[1] ? <AiFillEye /> : <AiFillEyeInvisible />}
+						</button>
+					</div>
+				</div>
+				<div className="text-center">
+					<button type="submit" className="py-2 px-4 rounded-lg bg-gray-700 text-white font-bold hover:bg-gray-600">
 						Register
 					</button>
 				</div>
